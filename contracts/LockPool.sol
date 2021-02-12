c4ac7ee13e3420d1cbbc9d29420fdec96ef6a583
0x112358 <gilfoyleproton@protonmail.com>
2021-02-12 12:57:20 +0300

    added proxy token

--- contracts/LockPool.sol
+++ contracts/LockPool.sol
@@ -2,16 +2,19 @@
 pragma solidity =0.6.6;
 
 import "./SyntheticToken.sol";
+import "./ProxyToken.sol";
 import "./time/Timeboundable.sol";
 
-contract LockPool is Timeboundable {
+contract LockPool is Timeboundable, ProxyToken {
     struct LockEvent {
         uint256 timestamp;
+        uint256 unlockDate;
         uint256 amount;
+        bool withdrawn;
     }
 
-    mapping(address => mapping(uint256 => LockEvent)) private lockEvents;
-    mapping(address => uint256) private lastWithdraw;
+    mapping(address => LockEvent[]) private lockEvents;
+    mapping(address => uint256) private lockEventLookupStart;
 
     SyntheticToken stakingToken;
     SyntheticToken rewardsToken;
@@ -20,12 +23,20 @@ contract LockPool is Timeboundable {
         uint256 _start,
         address _stakingToken,
         address _rewardsToken
-    ) public Timeboundable(_start, 0) {
+    ) public Timeboundable(_start, 0) ProxyToken(_stakingToken) {
         stakingToken = SyntheticToken(_stakingToken);
         rewardsToken = SyntheticToken(_rewardsToken);
     }
 
     function validTokenPermissions() public view returns (bool) {
         return rewardsToken.operator() == address(this);
+    }
+
+    function stake(uint256 amount) public override {
+        super.stake(amount);
+    }
+
+    function withdraw(uint256 amount) public override {
+        super.withdraw(amount);
     }
 }
