eec60105f72946ececd777524dd0403e0b8db8f0
0x112358 <gilfoyleproton@protonmail.com>
2021-02-20 12:58:19 +0300

    added balances to funds

--- dashboard/src/components/DevFund/DevFund.tsx
+++ dashboard/src/components/DevFund/DevFund.tsx
@@ -1,50 +1,31 @@
 import { Card, CardContent, CardHeader, CircularProgress, Grid } from '@material-ui/core';
 import { Alert } from '@material-ui/lab';
-import { ethers, BigNumber } from 'ethers';
+import { ethers } from 'ethers';
 import { useContext, useEffect, useState } from 'react';
 import { EthereumContext } from '../../App';
 import { toDate } from '../../lib/utils';
 import Entry from '../Entry';
-import stablefundabi from '../../data/stablefundabi.json';
+import devfundabi from '../../data/devfundabi.json';
 
-const StableFund = () => {
+const DevFund = () => {
   const { provider, registry, deployments } = useContext(EthereumContext);
   const [data, setData] = useState<any>(null);
   const [error, setError] = useState<any>(null);
   useEffect(() => {
     (async () => {
       if (!provider || !registry || !deployments) return;
+      const pool = new ethers.Contract(registry['DevFund'].address, devfundabi, provider);
       const kwbtc = new ethers.Contract(deployments['KWBTC'].address, deployments['KWBTC'].abi, provider);
-      const wbtc = new ethers.Contract(deployments['WBTC'].address, deployments['WBTC'].abi, provider);
-      const pool = new ethers.Contract(registry['StableFund'].address, stablefundabi, provider);
       try {
         const owner = await pool.owner();
         const operator = await pool.operator();
-        const pair = await pool.pair();
-        const router = await pool.router();
-        const tokenA = await pool.tokenA();
-        const tokenB = await pool.tokenB();
-        const trader = await pool.trader();
-
-        const balanceKWBTC = await kwbtc.balanceOf(registry['StableFund'].address);
-        const balanceWBTC = await kwbtc.balanceOf(registry['StableFund'].address);
-
-        const migrated = await pool.migrated();
+        const balanceKWBTC = await kwbtc.balanceOf(registry['DevFund'].address);
 
         const values = {
           owner,
           operator,
           blank1: null,
-          pair,
-          router,
-          tokenA,
-          tokenB,
-          trader,
-          blank2: null,
-          WBTC: balanceWBTC.toNumber() / 10 ** 8,
-          KBTC: balanceKWBTC.div(BigNumber.from(10).pow(10)).toNumber() / 10 ** 8,
-          blank3: null,
-          migrated,
+          kbtc: balanceKWBTC.toNumber() / 10 ** 8,
         };
         setData(values);
       } catch (e) {
@@ -56,7 +37,7 @@ const StableFund = () => {
   return (
     <Grid item xs={12} md={6} lg={6}>
       <Card>
-        <CardHeader title="StableFund" subheader={registry && <Entry v={registry['StableFund'].address} />} />
+        <CardHeader title="DevFund" subheader={registry && <Entry v={registry['DevFund'].address} />} />
         <CardContent>
           {error && <Alert severity="error">{`Error fetching pair data: ${error}`}</Alert>}
           {!data && !error && <CircularProgress />}
@@ -67,4 +48,4 @@ const StableFund = () => {
   );
 };
 
-export default StableFund;
+export default DevFund;
--- dashboard/src/pages/FundsPage/FundsPage.tsx
+++ dashboard/src/pages/FundsPage/FundsPage.tsx
@@ -1,10 +1,12 @@
 import { Grid } from '@material-ui/core';
+import DevFund from '../../components/DevFund';
 import StableFund from '../../components/StableFund';
 
 const FundsPage = () => {
   return (
     <Grid container spacing={3}>
       <StableFund />
+      <DevFund />
     </Grid>
   );
 };
