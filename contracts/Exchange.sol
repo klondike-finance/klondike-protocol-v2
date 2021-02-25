78f9284ae58442541bdff6ad1a3183a4db50d706
0x112358 <gilfoyleproton@protonmail.com>
2021-02-25 23:43:19 +0300

    added universal exchange

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,46 +1,54 @@
 //SPDX-License-Identifier: MIT
 pragma solidity =0.6.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 import "./access/Operatable.sol";
 import "./interfaces/ISmelter.sol";
 import "./libraries/UniswapLibrary.sol";
+import "./SyntheticToken.sol";
 
 contract Exchange is Operatable {
     uint256 constant ETH = 10**18;
     using SafeMath for uint256;
-    struct OraclePair {
+    struct TokenData {
         AggregatorV3Interface oracle0;
         AggregatorV3Interface oracle1;
-    }
-    struct TokenPair {
-        ERC20 token0;
-        ERC20 token1;
         ISmelter smelter;
     }
-    mapping(address => mapping(address => OraclePair)) public oracleIndex;
+    struct TokenPair {
+        SyntheticToken token0;
+        SyntheticToken token1;
+        
+    }
+    mapping(address => mapping(address => TokenData)) public tokenData;
     TokenPair[] public tokens;
 
-    function addPair(address tokenA, address tokenB, ISmelter smelter, address oracleA, address oracleB) public onlyOperator {
+    function addPair(address tokenA, address tokenB, address smelter, address oracleA, address oracleB) public onlyOperator {
         (address token0, address token1) = UniswapLibrary.sortTokens(tokenA, tokenB);
-        tokens.push(TokenPair({token0: ERC20(token0), toksen1: ERC20(token1), smelter: ISmelter}));
-        oracleIndex[tokenA][tokenB] = OraclePair({oracle0: AggregatorV3Interface(oracleA), oracle1: AggregatorV3Interface(oracleB)});
-        oracleIndex[tokenB][tokenA] = OraclePair({oracle0: AggregatorV3Interface(oracleB), oracle1: AggregatorV3Interface(oracleA)});
+        tokens.push(TokenPair({token0: SyntheticToken(token0), token1: SyntheticToken(token1)}));
+        tokenData[tokenA][tokenB] = TokenData({oracle0: AggregatorV3Interface(oracleA), oracle1: AggregatorV3Interface(oracleB), smelter: ISmelter(smelter)});
+        tokenData[tokenB][tokenA] = TokenData({oracle0: AggregatorV3Interface(oracleB), oracle1: AggregatorV3Interface(oracleA), smelter: ISmelter(smelter)});
     }
 
     function validPermissions() public view returns (bool) {
         for (uint i = 0; i < tokens.length; i++) {
-            if (tokens[i].token0.opetator() != address(tokens[i].smelter))|| (tokens[i].token1.opetator() != address(tokens[i].smelter)) {
+            ISmelter smelter = tokenData[address(tokens[i].token0)][address(tokens[i].token1)].smelter;
+            if (tokens[i].token0.operator() != address(smelter)) {
+                return false;
+            }
+            if (tokens[i].token1.operator() != address(smelter)) {
+                return false;
+            }
+            if (smelter.isTokenAdmin(address(this))) {
                 return false;
             }
         }
-        return tokenManager.isTokenAdmin(address(this));
+        return true;
     }
 
     function getPriceUnitAPerB(address tokenA, address tokenB) public view returns (uint256) {
-        OraclePair storage oraclePair = oracleIndex[tokenA][tokenB];
+        TokenData storage oraclePair = tokenData[tokenA][tokenB];
         if (address(oraclePair.oracle0) == address(0)) {
             return ETH;
         }
@@ -50,10 +58,13 @@ contract Exchange is Operatable {
     }
 
     function swap(address fromToken, address toToken, uint256 amountIn) public {
+        ISmelter smelter = tokenData[fromToken][toToken].smelter;
+        require(address(smelter) != address(0), "Exchange: Tokens are not managed");
+
         uint256 price = getPriceUnitAPerB(toToken, fromToken);
         uint256 amountOut = price.mul(amountIn).div(ETH);
-        tokenManager.burnSyntheticFrom(fromToken, msg.sender, amountIn);
-        tokenManager.mintSynthetic(toToken, msg.sender, amountOut);
+        smelter.burnSyntheticFrom(fromToken, msg.sender, amountIn);
+        smelter.mintSynthetic(toToken, msg.sender, amountOut);
         emit Swap(fromToken, toToken, amountIn, amountOut);
     }
 
