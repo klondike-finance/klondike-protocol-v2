5d75bebc2ae1ebb3d0b07f48af3f9def28abb786
0x112358 <gilfoyleproton@protonmail.com>
2021-02-28 18:16:15 +0300

    added swap

---
+++ contracts/Swap.sol
@@ -0,0 +1,69 @@
+//SPDX-License-Identifier: MIT
+pragma solidity =0.6.6;
+
+import "./SyntheticToken.sol";
+import "./access/Migratable.sol";
+import "./interfaces/ISmelter.sol";
+
+contract Swap is Operatable, Migratable {
+    mapping(address => address) public tokenIndex;
+    address[] public tokens;
+    address smelter;
+
+    constructor(address _smelter) public {
+        smelter = _smelter;
+    }
+
+    function validPermissions() public view returns (bool) {
+        for (uint256 i = 0; i < tokens.length; i++) {
+            ISmelter sm = ISmelter(smelter);
+            if (
+                SyntheticToken(tokenIndex[tokens[i]]).owner() !=
+                address(smelter)
+            ) {
+                return false;
+            }
+
+            if (!sm.isTokenAdmin(address(this))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function swap(
+        address inToken,
+        address outToken,
+        uint256 amount
+    ) public {
+        require(
+            tokenIndex[inToken] == outToken,
+            "Swap: token pair is not swappable"
+        );
+        SyntheticToken inT = SyntheticToken(inToken);
+        if (inT.owner() != address(smelter)) {
+            inT.transferFrom(
+                msg.sender,
+                0x000000000000000000000000000000000000dEaD,
+                amount
+            );
+        } else {
+            inT.burnFrom(msg.sender, amount);
+        }
+        SyntheticToken(outToken).mint(msg.sender, amount);
+    }
+
+    function setTokenPair(address inToken, address outToken)
+        public
+        onlyOperator
+    {
+        if (tokenIndex[inToken] == address(0)) {
+            tokens.push(inToken);
+        }
+        tokenIndex[inToken] = outToken;
+    }
+
+    function setSmelter(address _smelter) public onlyOperator {
+        smelter = _smelter;
+    }
+}
